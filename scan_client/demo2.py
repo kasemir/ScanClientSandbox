0431471d36fd3df166f35677baeae30e664309ed
kasemir <kasemirk@ornl.gov>
2015-03-19 09:39:27 -0400

    Demo for using another 'Set' in the same program.

--- scan_client/demo2.py
+++ scan_client/demo2.py
@@ -1,16 +1,31 @@
 """Demo 2
    
    Import into separate namespaces in case of
-   conflicts with other libraries that one might use
+   name conflicts with other libraries
 """
+
+# Assume you already have a 'Set' class.
+# That would conflict with the scan.commands.Set
+# if we simply used "from scan import *".
+class Set(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Can still use the 'other' set"
+
+# By importing the scan tools into their own namespace...
 import scan.client as client
 import scan.commands as cmd
 
+# .. you can use them ..
 client = client.ScanClient()
 cmds = [
     cmd.Comment("Hello"),
     cmd.Comment("World"),
     cmd.Set("voltage", 13.4)
 ]
-
 client.submit(cmds)
+
+# .. and not worry about clashing with other classes of the same name:
+print Set()
--- scan_client/scan/client.py
+++ scan_client/scan/client.py
@@ -3,5 +3,4 @@
 class ScanClient(object):
     
     def submit(self, commands):
-        print "Submitting scan:"
-        print ", ".join([ str(cmd) for cmd in commands ])
+        print "Submitting scan:", ", ".join([ str(cmd) for cmd in commands ])
